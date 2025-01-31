22954761e409e062ef70cb01815a74b793a33800
MLC Bloeiman <mar@strawmelonjuice.com>
2025-01-31 23:15:48 +0100

    Fix issue when trying to use a few params of different types

--- src/bun_sqlite_ffi.ts
+++ src/bun_sqlite_ffi.ts
@@ -1,4 +1,5 @@
 import { Database, type Statement } from "bun:sqlite";
+import * as gleam from "./gleam.mjs";
 
 export function Mexec(db: Database, query: string) {
   const res = db.exec(query);
@@ -22,19 +23,57 @@ export function Mprepare(db: Database, query: string): Statement<unknown> {
 
 export function Mrun(
   statement: Statement<unknown>,
-  params: unknown[],
+  params: string[],
 ): { last_insert_row_id: number | bigint; changes: number } {
   const res = statement.run(...params);
+  // return {
+  //   last_insert_row_id: res.lastInsertRowid,
+  //   changes: res.changes,
+  // };
   return {
-    last_insert_row_id: res.lastInsertRowid,
-    changes: res.changes,
+    last_insert_row_id: 0,
+    changes: 0,
   };
 }
 export function ParamArray_mnew(): unknown[] {
   return [];
 }
-export function ParamArray_mpush(arr: unknown[], val: unknown): unknown[] {
-  arr.push(val);
+
+// | JavaScript type | SQLite type |
+// | -------------- | ----------- |
+// | `string` | `TEXT` |
+// | `number` | `INTEGER` or `DECIMAL` |
+// | `boolean` | `INTEGER` (1 or 0) |
+// | `Uint8Array` | `BLOB` |
+// | `Buffer` | `BLOB` |
+// | `bigint` | `INTEGER` |
+// | `null` | `NULL` |
+
+export function ParamArray_mpush(
+  arr: string[],
+  val: string | number | boolean | Uint8Array | Buffer | bigint | null,
+): string[] {
+  let p: string;
+  switch (typeof val) {
+    case "string":
+      p = val;
+      break;
+    case "number":
+      p = val.toString();
+      break;
+    case "boolean":
+      p = val ? "1" : "0";
+      break;
+    case "bigint":
+      p = val.toString();
+      break;
+    case "undefined":
+      p = "NULL";
+      break;
+    default:
+      p = "NULL";
+  }
+  arr.push(p);
   return arr;
 }
 export function ParamArray_m_from_array(arr: unknown[]): unknown[] {
